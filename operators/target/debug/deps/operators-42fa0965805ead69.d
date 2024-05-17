54035037a962a66d5742cf888ddb88f8bc5db04e
HarryEddward <he0780070@gmail.com>
2024-05-17 12:45:59 +0200

    changes

---
+++ operators/target/debug/deps/operators-42fa0965805ead69.d
@@ -0,0 +1,5 @@
+/Users/yeray/Documents/RustLearning/RustLearning/operators/target/debug/deps/operators-42fa0965805ead69: src/main.rs
+
+/Users/yeray/Documents/RustLearning/RustLearning/operators/target/debug/deps/operators-42fa0965805ead69.d: src/main.rs
+
+src/main.rs:
---
+++ operators/target/debug/operators.d
@@ -0,0 +1,1 @@
+/Users/yeray/Documents/RustLearning/RustLearning/operators/target/debug/operators: /Users/yeray/Documents/RustLearning/RustLearning/operators/src/main.rs
